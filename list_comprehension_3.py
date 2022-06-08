34a1677485315eef16b563967dbcf2ee06ac0077
Manuel Cruz G <manuelcruzg78@gmail.com>
2022-06-07 22:39:01 -0500

    se adjunta el ejercicio #2 resuelto

---
+++ list_comprehension_3.py
@@ -0,0 +1,12 @@
+def run():
+    squares = []
+    for i in range(1, 101):
+        if i % 3 != 0:
+            squares.append(i**2)
+
+    print(squares)
+    
+
+
+if __name__ == '__main__':
+    run()
