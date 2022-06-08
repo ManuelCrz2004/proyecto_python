458040776446d9dda4ec63eb2d723e3df598023b
Manuel Cruz G <manuelcruzg78@gmail.com>
2022-06-07 21:52:25 -0500

    Se realiza un ejercicio de listas y diccionarios anidados en python

---
+++ listasDiccionarios.py
@@ -0,0 +1,28 @@
+def run():
+    my_list = [1, "Hello", True, 4.5]
+    my_dict = {"firstname": "Manuel", "lastname": "Cruz"}
+
+    super_list = [
+        {"firstname": "Manuel", "lastname": "Cruz"},
+        {"firstname": "Jeadran", "lastname": "Malagón"},
+        {"firstname": "Carolina", "lastname": "Garrote"},
+        {"firstname": "Victoria", "lastname": "Malagón"},
+        {"firstname": "Dali", "lastname": "De La Cruz"},
+    ]
+
+    super_dict = {
+        "natural_nums": [1, 2, 3, 4, 5], 
+        "integer_nums": [-1, -2, 0, 1, 2],
+        "floating_nums": [1.1, 4.5, 6.43]
+    }
+
+    for key, value in super_dict.items():
+        print("Super dictionary Values: ")
+        print(key, "-", value)
+    
+    for values in super_list:
+        for key, value in values.items():
+            print(f'{key} - {value}')
+
+if __name__ == '__main__':
+    run()
